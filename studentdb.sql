349832c595a4b22bd349c472d917002c5fa93cdb
gallazor <46328380+gallazor@users.noreply.github.com>
2021-05-12 15:28:30 +0200

    Update studentdb.sql

--- studentdb.sql
+++ studentdb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: May 11, 2021 at 09:09 AM
+-- Generation Time: May 12, 2021 at 01:28 PM
 -- Server version: 5.7.24
 -- PHP Version: 7.4.1
 
@@ -52,7 +52,7 @@ CREATE TABLE `students` (
   `studentid` int(11) NOT NULL,
   `firstname` varchar(128) NOT NULL,
   `surname` varchar(128) NOT NULL,
-  `class` varchar(128) NOT NULL,
+  `class_id` int(128) NOT NULL,
   `age` int(3) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -60,12 +60,12 @@ CREATE TABLE `students` (
 -- Dumping data for table `students`
 --
 
-INSERT INTO `students` (`studentid`, `firstname`, `surname`, `class`, `age`) VALUES
-(1, 'Allan', 'Hansen', 'IT', 24),
-(2, 'Lars', 'Monsen', 'musik', 18),
-(3, 'Berit', 'Ibsen', 'madlavning', 42),
-(4, 'John', 'Johnson', 'musik', 26),
-(5, 'Ellen', 'Gutson', 'IT', 21);
+INSERT INTO `students` (`studentid`, `firstname`, `surname`, `class_id`, `age`) VALUES
+(1, 'Allan', 'Hansen', 1, 24),
+(2, 'Lars', 'Monsen', 2, 18),
+(3, 'Berit', 'Ibsen', 3, 42),
+(4, 'John', 'Johnson', 2, 26),
+(5, 'Ellen', 'Gutson', 1, 21);
 
 --
 -- Indexes for dumped tables
@@ -81,7 +81,8 @@ ALTER TABLE `classes`
 -- Indexes for table `students`
 --
 ALTER TABLE `students`
-  ADD PRIMARY KEY (`studentid`);
+  ADD PRIMARY KEY (`studentid`),
+  ADD KEY `class_id` (`class_id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -98,6 +99,16 @@ ALTER TABLE `classes`
 --
 ALTER TABLE `students`
   MODIFY `studentid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `students`
+--
+ALTER TABLE `students`
+  ADD CONSTRAINT `class_id` FOREIGN KEY (`class_id`) REFERENCES `classes` (`class_id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
